87a26f3e25c9fe9e05d6803080bdc8099975dcae
Zack <zachary.chase@gmail.com>
2014-10-25 15:51:58 -0700

    added documentation file to organize structure of commands and utilities to be written

---
+++ node.py
@@ -0,0 +1,8 @@
+
+
+# represents a node in the network
+# needs to know if it is an input node, output node, or interior node
+
+class node:
+
+
---
+++ preprocessing.py
@@ -0,0 +1,18 @@
+
+
+# whitening
+# center all features at mean 0 and variance 1
+
+def whiten( matrix ):
+
+
+def whiten_mean( matrix ):
+
+
+def whiten_variance( matrix ):
+
+
+def whiten_variance_given_zero_mean(matrix):
+
+
+
